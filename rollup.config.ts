19c5c910464ca5c2ee9bc9afda5a97a08f40c0d8
peterthenelson <nelson.peter.a@gmail.com>
2024-11-03 11:45:21 -0500

    Rollup: cleaner and terser plugins

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,5 +1,7 @@
 import { RollupOptions } from 'rollup';
 import css from 'rollup-plugin-import-css';
+import cleaner from 'rollup-plugin-cleaner';
+import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import nodeResolve from '@rollup/plugin-node-resolve';
 
@@ -11,26 +13,29 @@ const pyodideImportStub = `(() => {
 function monacoWorker(path: string): RollupOptions {
   return {
     input: `node_modules/monaco-editor/esm/vs/${path}`,
-    output: { dir: 'server/public/bundle', format: 'es' }
+    output: { dir: 'server/public/bundle', format: 'es', plugins: [terser()] }
   };
 }
 
 // TODO: Add minification
 const config: RollupOptions[] = [
-  monacoWorker('editor/editor.worker.js'),
-  // NOTE: I'm leaving out language/{css,html,json,typescript}/{css,html,json,ts}.worker.js
   {
     input: 'web-host/index.ts',
     output: {
       dir: 'server/public/bundle',
+      sourcemap: true,
       format: 'esm'
     },
     plugins: [
+      cleaner({ targets: ['server/public/bundle'] }),
       css({ output: 'included.css' }),
       typescript(),
       nodeResolve(),
+      terser(),
     ]
   },
+  monacoWorker('editor/editor.worker.js'),
+  // NOTE: I'm leaving out language/{css,html,json,typescript}/{css,html,json,ts}.worker.js
   {
     // NOTE: Not sure this is really the best way to swap the local import for
     // importScripts(cdnUrl), but... it works.
@@ -39,11 +44,12 @@ const config: RollupOptions[] = [
     output: {
       file: 'server/public/bundle/worker.js',
       format: 'iife',
+      sourcemap: true,
       globals: {
         pyodide: pyodideImportStub
       },
     },
-    plugins: [typescript()]
+    plugins: [typescript(), terser()],
   },
 ];
 
