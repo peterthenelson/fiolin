8fc22ea9adccbf2c6c31c6e6e927e68e7bac49fb
peterthenelson <nelson.peter.a@gmail.com>
2024-11-25 23:24:57 -0500

    Prepping for multi-step tutorial

---
+++ tutorial/welcome.py
@@ -0,0 +1,12 @@
+# Basic script that copies input to output
+import fiolin
+import os
+
+input = fiolin.get_input_basename()
+stem, ext = os.path.splitext(input)
+
+# Try changing the output filename (foo.txt -> foo-copy.txt or FOO.txt)
+output = stem + ext
+
+print(f'Copying /input/{input} to /output/{output}')
+fiolin.cp(f'/input/{input}', f'/output/{output}')
