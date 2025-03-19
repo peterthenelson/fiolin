3bb36d1be73ad0b671debbd4b442574bc08ddfeb
peterthenelson <nelson.peter.a@gmail.com>
2025-03-19 19:46:30 -0400

    Begin rendering/init test

---
+++ components/test/container.test.ts
@@ -0,0 +1,28 @@
+/**
+ * @vitest-environment happy-dom
+ */
+import { describe, expect, it } from 'vitest';
+import { renderContainer, FiolinContainerOptions } from '../server/container';
+import { dedent } from '../../common/indent';
+import { loadScript } from '../../utils/config';
+
+describe('container', () => {
+  describe('rendering and initialization', () => {
+    it('works for 1p', () => {
+      const serverOpts: FiolinContainerOptions = {
+        mode: '1P',
+        script: loadScript('extract-winmail'),
+        numSpaces: 4,
+      };
+      document.write(dedent(`
+        <html>
+          <body>
+            ${renderContainer(serverOpts)}
+          </body>
+        </html>
+      `));
+      // TODO: client initialization
+    });
+    // TODO: playground
+  });
+});
