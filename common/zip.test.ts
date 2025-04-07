cfa1e7feba6dd732cbde66ddbecb60e24026d44b
peterthenelson <nelson.peter.a@gmail.com>
2025-04-07 10:15:38 -0400

    Multizip

--- common/zip.test.ts
+++ common/zip.test.ts
@@ -18,4 +18,23 @@ describe('zip', () => {
     files.sort();
     expect(files).toEqual(['bar.txt', 'baz.txt', 'foo.txt']);
   });
+
+  it('zips and preserves directory structure', async () => {
+    const foo = mkFile('alpha/foo.txt', 'foo');
+    const bar = mkFile('beta/gamma/bar.txt', 'bar');
+    const baz = mkFile('beta/delta/baz.txt', 'baz');
+    const zip = await zipFiles([foo, bar, baz]);
+    writeFileSync(`${output.path}/${zip.name}`, Buffer.from(await zip.arrayBuffer()));
+    const files = await listZip(`${output.path}/${zip.name}`);
+    files.sort();
+    expect(files).toEqual([
+      'alpha/',
+      'alpha/foo.txt',
+      'beta/',
+      'beta/delta/',
+      'beta/delta/baz.txt',
+      'beta/gamma/',
+      'beta/gamma/bar.txt'
+    ]);
+  });
 });
--- common/zip.ts
+++ common/zip.ts
@@ -1,8 +1,22 @@
 import JSZip from 'jszip';
 
-// TODO: Make this work with folders
+function folders(paths: string[]): string[] {
+  const dirs = new Set<string>();
+  for (const path of paths) {
+    const parts = path.split('/');
+    for (let i = 0; i < parts.length - 1; i++) {
+      const dirPath = parts.slice(0, i + 1).join('/');
+      dirs.add(dirPath);
+    }
+  };
+  return Array.from(dirs);
+}
+
 export async function zipFilesRaw(pathAndContents: [string, Promise<ArrayBuffer>][]): Promise<File> {
   const zip = new JSZip();
+  for (const dir of folders(pathAndContents.map((x) => x[0]))) {
+    zip.folder(dir);
+  }
   for (const [path, bufPromise] of pathAndContents) {
     zip.file(path, bufPromise)
   }
